dae456c50156c0a2e9c8f77c6fe9cc593950014d
robert <roberthshields93@yahoo.com>
2021-01-22 11:20:02 -0800

    file validation

--- src/COMPONENTS/FileUpload.js
+++ src/COMPONENTS/FileUpload.js
@@ -1,12 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 const FileUpload = () => {
 
     const [list, setList] = useState([])
-
-    useEffect(() => {
-
-    }, [])
 
     var file = null;
     const detectFile = () => {
@@ -17,7 +13,7 @@ const FileUpload = () => {
 
     const processFile = () => {
         // If selected file is not null
-        if (file) {
+        if (file && file.name.includes('csv')) {
             console.log(`Processing: ${file.name}`)
             // Create FileReader obj to turn file data into a string
             var fileReader = new FileReader();
@@ -26,6 +22,8 @@ const FileUpload = () => {
             fileReader.onload = (e) => {
                 processData(e.target.result);
             }
+        } else {
+            alert('I only eat .csv files (comma delimited)!')
         }
     }
 
@@ -34,65 +32,76 @@ const FileUpload = () => {
     const processData = (data) => {
         // Split data into an array of strings to work with
         var lines = data.split(/[\r\n]+/g);
-        // Iterate thru each string to extract data points
-        for (let i = 1; i < lines.length -1; i++) {
-            // Split current string into array of strings to work with
-            var datapoints = lines[i].split(',')
-
-            // Declare array variable that will store current parcel's data
-            var parcelData = []
-            // Declare offset variable to handle unexpected commas
-            var offset = 0;
-
-            // Parcel_PK
-            var key = datapoints[0 + offset]
-            parcelData.key = key
-
-            // Column that sometimes contains a comma, so offset needs to be incremented
-            if (datapoints[2 + offset].includes('"') && datapoints[2 + offset].indexOf('"') === 0)
-                offset += 1
-            // Column that sometimes contains a comma, so offset needs to be incremented
-            if (datapoints[6 + offset].includes('"') && datapoints[6 + offset].indexOf('"') === 0)
-                offset += 1
-
-            // Address
-            var address = datapoints[4 + offset]
-            parcelData.address = address
-            // City
-            var city = datapoints[5 + offset]
-            parcelData.city = city
-            
-            // Owner Name(s) -- this one is tricky due to the varying # of owners
-            var owner = datapoints[17 + offset]
-            if (datapoints[17 + offset].includes('"')) {
-                while (owner.charAt(owner.length - 1) !== '"') {
-                    offset += 1
-                    owner += datapoints[17 + offset]
-                }
+        
+        // Validation!! checking that at least the first col name is 'PARCEL_PK'
+        var colHeaders = lines[0].split(',')
+        if (!colHeaders[0].includes('PARCEL_PK')) {
+            if (colHeaders[26] && !colHeaders[26].includes('NEIGHBORHOOD')) {
+                alert('.csv data not in expected format :(')
             }
-            owner = owner.replaceAll('&amp;', '&');
-            owner = owner.replaceAll('"', '');
-            parcelData.owner = owner
+            alert('.csv data definitely not in expected format :(')
+        } else {
+            // Iterate thru each string to extract data points
+            for (let i = 1; i < lines.length -1; i++) {
+                // Split current string into array of strings to work with
+                var datapoints = lines[i].split(',')
 
-            // Column that wouldn't surprise me if it contained a comma, so offset would need to be incremented
-            if (datapoints[18 + offset].includes('"') && datapoints[18 + offset].indexOf('"') === 0)
-                offset += 1
-            // Owner Address
-            var ownerAddress = datapoints[18 + offset]
-            parcelData.ownerAddress = ownerAddress
-            // Owner State
-            var ownerState = datapoints[19 + offset]
-            parcelData.ownerState = ownerState
-            // Owner Zip
-            var ownerZip = datapoints[20 + offset]
-            parcelData.ownerZip = ownerZip
-            // console.log(i+1 + "  >  " + ownerState + "  >  " + ownerZip)
-            processedData.push(parcelData)
-            
+                // Declare array variable that will store current parcel's data
+                var parcelData = []
+                // Declare offset variable to handle unexpected commas
+                var offset = 0;
+
+                // Parcel_PK
+                var key = datapoints[0 + offset]
+                parcelData.key = key
+
+                // Column that sometimes contains a comma, so offset needs to be incremented
+                if (datapoints[2 + offset].includes('"') && datapoints[2 + offset].indexOf('"') === 0)
+                    offset += 1
+                // Column that sometimes contains a comma, so offset needs to be incremented
+                if (datapoints[6 + offset].includes('"') && datapoints[6 + offset].indexOf('"') === 0)
+                    offset += 1
+
+                // Address
+                var address = datapoints[4 + offset]
+                parcelData.address = address
+                // City
+                var city = datapoints[5 + offset]
+                parcelData.city = city
+                
+                // Owner Name(s) -- this one is tricky due to the varying # of owners
+                var owner = datapoints[17 + offset]
+                if (datapoints[17 + offset].includes('"')) {
+                    while (owner.charAt(owner.length - 1) !== '"') {
+                        offset += 1
+                        owner += datapoints[17 + offset]
+                    }
+                }
+                owner = owner.replaceAll('&amp;', '&');
+                owner = owner.replaceAll('"', '');
+                parcelData.owner = owner
+
+                // Column that wouldn't surprise me if it contained a comma, so offset would need to be incremented
+                if (datapoints[18 + offset].includes('"') && datapoints[18 + offset].indexOf('"') === 0)
+                    offset += 1
+                // Owner Address
+                var ownerAddress = datapoints[18 + offset]
+                parcelData.ownerAddress = ownerAddress
+                // Owner State
+                var ownerState = datapoints[19 + offset]
+                parcelData.ownerState = ownerState
+                // Owner Zip
+                var ownerZip = datapoints[20 + offset]
+                parcelData.ownerZip = ownerZip
+                // console.log(i+1 + "  >  " + ownerState + "  >  " + ownerZip)
+                processedData.push(parcelData)
+                
+            }
+            console.log(processedData);
+            setList(processedData)
         }
-        console.log(processedData);
-        setList(processedData)
     }
+
     const searchFilter = (e) => {
         console.log(e.target.value.toUpperCase())
         var results = document.querySelectorAll('.data-row');
